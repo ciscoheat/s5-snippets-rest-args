6e440c61d96e075df169991ff52040bbb70b9a68
Andreas Söderlund <ciscoheat@gmail.com>
2024-05-05 21:21:12 +0200

    first commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,8 @@
+<script lang="ts"></script>
+
+<!-- Remove the whitespace before the colon to remove error -->
+{#snippet foo(...test: string[])}
+	<button>{test.join(", ")}</button>
+{/snippet}
+{@render foo(["a", "b"])}
+
